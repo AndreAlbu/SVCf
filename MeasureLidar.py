0d5284093fc4e1042704fbb4363a5a7132ead004
Alan Paulino <alanvictor5447@gmail.com>
2023-05-25 18:08:47 -0300

    Last update before presentation

--- MeasureLidar.py
+++ MeasureLidar.py
@@ -35,6 +35,7 @@ def showTwoImages(image1, image2):
     plt.show()
     cv.waitKey(0)
 
+
 def organizaTXT(arquivo):
     arq = open(arquivo)
     lines = arq.readlines()
@@ -51,38 +52,34 @@ def organizaTXT(arquivo):
     return pontos
 
 
-def main():
-    import matplotlib
-    matplotlib.use('MacOSX')
+def calibrationLIDAR(pontos, imageOriginal, imageLidar):
+    ## Eixo vertical
+    ponto1 = [0, 127]
+    ponto2 = [191, 127]
 
-    baseURL = "/Users/alanpaulino/projects/Pack estudo lidar/"
-    numberImage = "5"
+    ## Eixo horizontal
+    # ponto1 = [95, 0]
+    # ponto2 = [95, 255]
 
-    pontos = organizaTXT(baseURL + numberImage + "_depth.txt")
+    sizeHeight = 0.168
+    sizeWidth = 0.248
 
-    ponto1 = [60, 131]
-    ponto2 = [75, 131]
+    x = float(pontos[ponto1[0]][ponto1[1]])
+    print(str(x) + " m")
+    y = float(pontos[ponto2[0]][ponto2[1]])
+    print(str(y) + " m")
 
-    # sizeHeight = 0.26
-    # sizeHeight = 0.465
-    # sizeWidth = 0.35
-    # sizeWidth = 0.635
+    angle = np.arccos((x**2+y**2-sizeHeight**2)/(2*x*y))
+    print(angle)
+    print(np.rad2deg(angle))
+    ## valor achado vertical 57.87424017358846
+    ## valor achado horizontal 81.27321528320013
 
-    # x = float(pontos[ponto1[0]][ponto1[1]])
-    # print(x)
-    # y = float(pontos[ponto2[0]][ponto2[1]])
-    # print(y)
-    # angle = np.arccos((x**2+y**2-sizeHeight**2)/(2*x*y))
-    # print(angle)
-    # print(np.rad2deg(angle))
     # a = 58.24929668917284
     # b = 59.01806172559483
     # media = (a + b) / 2
 
-    x = float(pontos[ponto1[0]][ponto1[1]])
-    print(x)
-    y = float(pontos[ponto2[0]][ponto2[1]])
-    print(y)
+
     # angle = np.arccos((x ** 2 + y ** 2 - sizeWidth ** 2) / (2 * x * y))
     # print(angle)
     # print(np.rad2deg(angle))
@@ -91,8 +88,17 @@ def main():
     # media = (a + b) / 2
     # print(media)
 
-    xGrau = 81.86114541269245 * ponto1[1] / 256
-    yGrau = 58.633679207383835 * ponto1[0] / 192
+    cv.circle(imageOriginal, (ponto1[1], ponto1[0]), 0, (0, 0, 255), -1)
+    cv.circle(imageOriginal, (ponto2[1], ponto2[0]), 0, (255, 0, 0), -1)
+    cv.circle(imageLidar, (ponto1[1], ponto1[0]), 0, (0, 0, 255), -1)
+    cv.circle(imageLidar, (ponto2[1], ponto2[0]), 0, (255, 0, 0), -1)
+    showTwoImages(imageOriginal, imageLidar)
+
+
+def measureDistance(ponto1, ponto2, pontos, image, imageLidar):
+
+    xGrau = 81.27321528320013 * ponto1[1] / 256
+    yGrau = 57.87424017358846 * ponto1[0] / 192
     ponto1Grau = [yGrau, xGrau]
 
     xGrau = 81.86114541269245 * ponto2[1] / 256
@@ -100,21 +106,48 @@ def main():
     ponto2Grau = [yGrau, xGrau]
 
     angle = np.sqrt((ponto2Grau[0] - ponto1Grau[0]) ** 2 + (ponto2Grau[1] - ponto1Grau[1]) ** 2)
-    print("O angulo encontrado é: " + str(angle))
+    print("O angulo encontrado é: " + str(angle) + " graus")
+
+    x = float(pontos[ponto1[0]][ponto1[1]])
+    print(str(x) + " m")
+    y = float(pontos[ponto2[0]][ponto2[1]])
+    print(str(y) + " m")
 
     size = np.sqrt(x ** 2 + y ** 2 - (2 * x * y * np.cos(np.deg2rad(angle))))
     print("O valor calculado é: " + str(size * 100) + " cm")
+
+    cv.circle(image, (ponto1[1], ponto1[0]), 0, (0, 0, 255), -1)
+    cv.circle(image, (ponto2[1], ponto2[0]), 0, (255, 0, 0), -1)
+    cv.circle(imageLidar, (ponto1[1], ponto1[0]), 0, (0, 0, 255), -1)
+    cv.circle(imageLidar, (ponto2[1], ponto2[0]), 0, (255, 0, 0), -1)
+    showTwoImages(image, imageLidar)
+
+
+
+def main():
+    import matplotlib
+    matplotlib.use('MacOSX')
+
+    baseURL = "/Users/alanpaulino/projects/NewCapture/Demo/"
+    numberImage = "3"
+
+    pontos = organizaTXT(baseURL + numberImage + "_depth.txt")
 
     imageOriginal = cv.imread(baseURL + numberImage + ".jpg")
     image = resizeImage(imageOriginal, 7.5)
 
     imageLidar = cv.imread(baseURL + numberImage + "_imageDepth.jpg")
 
-    cv.circle(image, (ponto1[1], ponto1[0]), 1, (0, 0, 255), -1)
-    cv.circle(image, (ponto2[1], ponto2[0]), 1, (255, 0, 0), -1)
-    cv.circle(imageLidar, (ponto1[1], ponto1[0]), 0, (0, 0, 255), -1)
-    cv.circle(imageLidar, (ponto2[1], ponto2[0]), 0, (255, 0, 0), -1)
-    showTwoImages(image, imageLidar)
+    ponto1 = [102, 115]
+    ponto2 = [96, 123]
+
+    # ponto1 = [75, 129]
+    # ponto2 = [82, 129]
+
+    # calibrationLIDAR(pontos, image, imageLidar)
+    measureDistance(ponto1, ponto2, pontos, image, imageLidar)
+
+
 
 if __name__ == "__main__":
     main()
